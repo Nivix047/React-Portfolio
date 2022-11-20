fc9474cb8256d20ab40197dc4f3a125f70e9887f
Daniel Xu <xudh47@gmail.com>
2022-11-20 15:26:31 -0800

    Updated contact page

--- src/compenents/pages/Contact.js
+++ src/compenents/pages/Contact.js
@@ -2,9 +2,31 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <div className="m-5">
+    <div className="m-5 w-50">
       <h1>Contact</h1>
-      <p>Test</p>
+      <form>
+        <div className="form-group m-3">
+          <label>Name:</label>
+          <textarea className="form-control" rows="1"></textarea>
+        </div>
+        <div className="form-group m-3">
+          <label>Email address:</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="name@example.com"
+          />
+        </div>
+        <div className="form-group m-3">
+          <label>Message:</label>
+          <textarea className="form-control" rows="10"></textarea>
+        </div>
+        <div class="form-group m-3">
+          <button type="submit" class="btn btn-dark">
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
