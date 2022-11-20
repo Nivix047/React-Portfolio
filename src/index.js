73cc3b8af4aac8fb7cc9b293e7cc9934a45847e6
Daniel Xu <xudh47@gmail.com>
2022-11-19 23:48:58 -0800

    First commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import RectDOM from "react-dom";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+React.DOM.render(<App />, document.getElementById("root"));
