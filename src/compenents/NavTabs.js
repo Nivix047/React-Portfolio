fffdd2709e779aeac93db1379d02b6d98134e018
Daniel Xu <xudh47@gmail.com>
2022-11-21 00:26:13 -0800

    Updated CSS

--- src/compenents/NavTabs.js
+++ src/compenents/NavTabs.js
@@ -7,17 +7,26 @@ function NavTabs({ handlePageChange }) {
         className="navbar navbar-expand-lg navbar-light bg-light"
         id="navbar"
       >
+        <a
+          className="home navbar-brand"
+          href="#home"
+          onClick={() => handlePageChange("Home")}
+        >
+          About me
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a
-                className="nav-link"
-                href="#home"
-                onClick={() => handlePageChange("Home")}
-              >
-                About me <span className="sr-only"></span>
-              </a>
-            </li>
             <li className="nav-item">
               <a
                 className="nav-link"
