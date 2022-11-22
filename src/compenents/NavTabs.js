68542a1967f7b32f8b628cb29dd6a781e271a62d
Daniel Xu <xudh47@gmail.com>
2022-11-21 22:42:31 -0800

    Fixed navbar hamburger dropdown bug

--- src/compenents/NavTabs.js
+++ src/compenents/NavTabs.js
@@ -3,62 +3,120 @@ import React from "react";
 function NavTabs({ handlePageChange }) {
   return (
     <div>
-      <nav
-        className="navbar navbar-expand-lg navbar-light bg-light"
-        id="navbar"
-      >
-        <a
-          className="home navbar-brand"
-          href="#home"
-          onClick={() => handlePageChange("Home")}
-        >
-          About me
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="#portfolio"
-                onClick={() => handlePageChange("Portfolio")}
-              >
-                Portfolio
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="#contact"
-                onClick={() => handlePageChange("Contact")}
-              >
-                Contact Me
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="#resume"
-                onClick={() => handlePageChange("Resume")}
-              >
-                Resume
-              </a>
-            </li>
-          </ul>
+      <nav class="navbar navbar-expand-lg bg-light" id="navbar">
+        <div className="container-fluid">
+          <a
+            className="navbar-brand"
+            href="home"
+            onClick={() => handlePageChange("Home")}
+          >
+            About me
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="#home"
+                  onClick={() => handlePageChange("Portfolio")}
+                >
+                  Portfolio
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="#contact"
+                  onClick={() => handlePageChange("Home")}
+                >
+                  Contact me
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="#resume"
+                  onClick={() => handlePageChange("Resume")}
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
+      ;
     </div>
   );
 }
 
 export default NavTabs;
+
+{
+  /* <nav
+className="navbar navbar-expand-lg navbar-light bg-light"
+id="navbar"
+>
+<a
+  className="home navbar-brand"
+  href="#home"
+  onClick={() => handlePageChange("Home")}
+>
+  About me
+</a>
+<button
+  className="navbar-toggler"
+  type="button"
+  data-bs-toggle="collapse"
+  data-bs-target="navbarNav"
+  aria-controls="navbarNav"
+  aria-expanded="false"
+  aria-label="Toggle navigation"
+>
+  <span className="navbar-toggler-icon"></span>
+</button>
+<div className="collapse navbar-collapse" id="navbarNav">
+  <ul className="navbar-nav">
+    <li className="nav-item">
+      <a
+        className="nav-link"
+        href="#portfolio"
+        onClick={() => handlePageChange("Portfolio")}
+      >
+        Portfolio
+      </a>
+    </li>
+    <li className="nav-item">
+      <a
+        className="nav-link"
+        href="#contact"
+        onClick={() => handlePageChange("Contact")}
+      >
+        Contact Me
+      </a>
+    </li>
+    <li className="nav-item">
+      <a
+        className="nav-link"
+        href="#resume"
+        onClick={() => handlePageChange("Resume")}
+      >
+        Resume
+      </a>
+    </li>
+  </ul>
+</div>
+</nav> */
+}
