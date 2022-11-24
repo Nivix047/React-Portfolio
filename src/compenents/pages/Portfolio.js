2fbd0d611bc75da828a20e01268f731466eca964
Daniel Xu <xudh47@gmail.com>
2022-11-23 22:37:39 -0800

    Added list key to map function

--- src/compenents/pages/Portfolio.js
+++ src/compenents/pages/Portfolio.js
@@ -28,61 +28,10 @@ export default function Portfolio() {
     <div className="container">
       <h3>Portfolio</h3>
       <div className="row">
-        {data.map((project) => {
-          return <Project data={project} />;
+        {data.map((project, index) => {
+          return <Project key={index} data={project} />;
         })}
       </div>
     </div>
   );
-}
-
-{
-  /* <div className="container">
-<div className="row">
-  <div
-    className="projects col m-3 border border black"
-    id="project-one"
-  >
-    <h1>
-      <a href="https://katarinamihaylovich.github.io/film-buzz/">
-        Film Buzz
-      </a>
-    </h1>
-  </div>
-  <div
-    className="projects col m-3 border border black"
-    id="project-two"
-  >
-    <h1>
-      <a href="https://jda-bank.herokuapp.com/login">JDA Bank</a>
-    </h1>
-  </div>
-  <div className="w-100"></div>
-  <div
-    className="projects col m-3 border border black"
-    id="project-three"
-  >
-    <h1>Project 3</h1>
-  </div>
-  <div
-    className="projects col m-3 border border black"
-    id="project-four"
-  >
-    <h1>Project 4</h1>
-  </div>
-  <div className="w-100"></div>
-  <div
-    className="projects col m-3 border border black"
-    id="project-five"
-  >
-    <h1>Project 5</h1>
-  </div>
-  <div
-    className="projects col m-3 border border black"
-    id="project-six"
-  >
-    <h1>Project 6</h1>
-  </div>
-</div>
-</div> */
 }
